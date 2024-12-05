f81711b0e394b39ff20d6b7960178a7cc324e75d
giyomdabin <dabinjang01@gmail.com>
2024-12-05 22:16:26 +0900

    feat: 사용자의 체력 분석 기능 추가

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,15 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
     # .env 파일 로드
     load_dotenv()
-
     # 환경 변수 설정
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-
     # 확장 초기화
     db.init_app(app)
+    # Blueprint 등록
+    from app.routes.fitness_routes import fitness_bp
+    app.register_blueprint(fitness_bp)
 
     # Blueprint 등록
     from app.routes.exercise_routes import exercise_bp  # Blueprint 임포트
